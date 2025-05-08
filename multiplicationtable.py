7cbf97029dafed91bc3298dcb19fc361a0252d1d
tanmayhande12 <74094872+tanmayhande12@users.noreply.github.com>
2025-05-08 10:20:26 +0530

    Add files via upload

---
+++ multiplicationtable.py
@@ -0,0 +1,7 @@
+multiplier = 5
+counter = 1
+
+while counter <= 10:
+    result = counter * multiplier
+    print(f"{counter} x {multiplier} = {result}")
+    counter += 1
---
+++ squarerootfrom1to20.py
@@ -0,0 +1,6 @@
+number = 1 
+ 
+while number <= 10: 
+    square = number ** 2 
+    print(f"The square of {number} is {square}") 
+    number += 1 
