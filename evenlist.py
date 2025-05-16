e2441afed1e2c17c9fc4f13c6cea205a85eae3e4
tanmayhande12 <74094872+tanmayhande12@users.noreply.github.com>
2025-05-16 11:22:28 +0530

    Delete evenlist.py

--- evenlist.py
+++
@@ -1,6 +0,0 @@
-def remove_even_numbers(input_list):
-    odd_numbers = [num for num in input_list if num % 2 != 0]
-    return odd_numbers
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result_list = remove_even_numbers(my_list)
-print(result_list)
